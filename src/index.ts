462492dcfe8ff6b694bae52c1da54ac7a85a0111
CCXXXI <ccxxxi@qq.com>
2022-05-18 06:19:33 +0800

    feat: match tweet url

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import {Client, Intents} from "discord.js";
 import * as token from "./token.json";
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
+const tweetUrl = /https:\/\/twitter\.com\/\w+\/status\/\d+/;
+client.on("messageCreate", (message) => {
+    const match = message.content.match(tweetUrl);
+    if (match !== null) {
+        const url = match[0];
+        message.channel.send(`Got <${url}>`);
+    }
+});
+
+// noinspection JSIgnoredPromiseFromCall
 client.login(token);
