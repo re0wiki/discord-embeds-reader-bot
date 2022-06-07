6b683726c982714f660df9105254b9672b12c2e4
CCXXXI <ccxxxi@qq.com>
2022-06-07 13:27:35 +0800

    feat: logger and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Client, Intents, Message } from "discord.js";
 import * as token from "./token.json";
-import { sleep } from "./utils";
+import { logger, sleep } from "./utils";
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -8,30 +8,47 @@ const client = new Client({
 
 client.on("messageCreate", async (msg) => {
   // Ignore messages from myself
-  if (msg.author == client.user) return;
+  if (msg.author == client.user) {
+    logger.debug(`From myself: ${msg.content}`);
+    return;
+  }
 
   // Wait for the embeds to appear
   await sleep(5000);
 
-  // Handle massages without embeds
-  if (msg.embeds.length === 0) {
-    console.log(`No embeds: ${msg.content}`);
-    return;
-  }
-
-  // Send embeds as normal messages
-  const sent: Message[] = [];
-  sent.push(await msg.channel.send(msg.embeds[0].description));
+  // Convert the embeds to text and images
+  const text = msg.embeds
+    .map((e) => e.description)
+    .filter((d) => d.length > 0)
+    .join("\n");
   const images = msg.embeds
     .map((e) => e.image)
     .filter((img) => img !== null)
     .map((img) => img.url);
-  if (images.length !== 0) sent.push(await msg.channel.send({ files: images }));
 
-  // Delete sent messages after a few seconds. They are useless for discord users.
+  // Handle massages without embeds
+  if (text.length === 0 && images.length === 0) {
+    logger.debug(`No embeds: ${msg.content}`);
+    return;
+  }
+
+  // Send the converted message
+  let sent: Message;
+  if (images.length === 0) {
+    logger.info(`Sending text: ${text}`);
+    sent = await msg.channel.send(text);
+  } else if (text.length === 0) {
+    logger.info(`Sending images: ${images}`);
+    sent = await msg.channel.send({ files: images });
+  } else {
+    logger.info(`Sending text and ${images.length} images: ${text} ${images}`);
+    sent = await msg.channel.send({ content: text, files: images });
+  }
+
+  // Delete the converted message after a few seconds.
+  // They are useless for discord users.
   await sleep(10000);
-  sent.forEach((msg) => msg.delete());
+  await sent.delete();
 });
 
-// noinspection JSIgnoredPromiseFromCall
-client.login(token);
+client.login(token).then(() => logger.info("Logged in"));
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,26 @@
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+  level: "debug",
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.timestamp(),
+        format.colorize(),
+        format.simple()
+      ),
+    }),
+    new transports.File({
+      filename: "discord-embeds-reader-bot.log",
+      format: format.combine(
+        format.timestamp(),
+        format.errors({ stack: true }),
+        format.simple()
+      ),
+    }),
+  ],
+});
+
 /**
  * @see https://stackoverflow.com/a/39914235/13805358
  */
@@ -5,4 +28,4 @@ async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export { sleep };
+export { logger, sleep };
