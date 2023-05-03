1d9871cc8cc0819228f6aeb40cf5de4d9902748b
CCXXXI <ccxxxi@qq.com>
2023-05-03 14:08:28 +0800

    fix: null check

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ client.on("messageCreate", async (msg) => {
 
     // Convert the embeds to text and images.
     const textArr = msg.embeds
-        .flatMap((e) => [e.author.name, e.title, e.description, e.url])
+        .flatMap((e) => [e.author?.name, e.title, e.description, e.url])
         .filter((s) => s !== null && s.length > 0);
     const text = unique(textArr)
         .join("\n");
